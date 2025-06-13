5cd83535428b338a1e3af15b6278981411682c75
Chen-Oku <chenoku@gmail.com>
2025-06-13 13:28:41 -0500

    Revert "Inventory Scripts"
    This reverts commit 28c5457515121a5ac3fc40f846f53bb1b50aa9be.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -56,10 +56,6 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Si el inventario está abierto, no procesar input de movimiento
-        if (FindFirstObjectByType<Inventory>()?.inventoryEnabled == true)
-            return;
-
         // Comprobar si está en el suelo
         isGrounded = charController.isGrounded;
         if (isGrounded && velocity.y < 0)
@@ -78,10 +74,9 @@ public class PlayerController : MonoBehaviour
             HandleMovementInput();
             HandleJumpInput();
         }
-        
 
         HandleDashInput();
-        HandleSliceInput();
+            HandleSliceInput();
         HandleCrouchInput();
         HandleInteractInput();
 
